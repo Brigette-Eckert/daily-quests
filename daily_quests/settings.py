56997977050b1df5ad1667b433129cb094b6e14a
Brigette Eckert <Brigette.Eckert@live.com>
2017-08-06 10:12:27 -0700

    #9 changed settings.py file -got static files to load properly

--- daily_quests/settings.py
+++ daily_quests/settings.py
@@ -114,6 +114,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/'
 ]
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
--- daily_quests/urls.py
+++ daily_quests/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from rest_framework import routers
 from . import views
 
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'quests', views.QuestViewSet)
@@ -30,3 +31,5 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+
